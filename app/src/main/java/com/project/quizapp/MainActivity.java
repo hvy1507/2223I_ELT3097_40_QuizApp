f6897582fbd89dc41bb59dfe9aebb7f03291d116
Quoc Huy <ntinh2584@gmail.com>
2022-12-01 15:16:36 +0700

    Update tool + navigation bar + Fragment Home

--- app/src/main/java/com/project/quizapp/MainActivity.java
+++ app/src/main/java/com/project/quizapp/MainActivity.java
@@ -3,12 +3,18 @@ package com.project.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentHome fragmentHome = new FragmentHome();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tool_bar, menu);
+        return true;
     }
 }
